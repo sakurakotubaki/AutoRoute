7aefca22714c55316a89535bbe28fc66f33f407b
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-07-24 22:44:48 +0900

    delete api keyを削除　

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
-import 'package:auto_route_tutorial/route/app_router.dart';
+import 'package:auto_route_tutorial/firebase_options.dart';
+import 'package:auto_route_tutorial/routes/router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyApp extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    // AppRouterクラスをインスタンス化
-    AppRouter appRouter = AppRouter();
-    return MaterialApp.router(// MaterialApp.routerを使用
-      routerConfig: appRouter.config(),
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _appRouter = ref.watch(appRouterProvider);// appRouterProviderは、ルーティングの設定を管理する
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerDelegate: _appRouter.delegate(),// routerDelegateは、ルーティングの状態を管理する
+      routeInformationParser: _appRouter.defaultRouteParser(),// routeInformationParserは、URLをルーティングに変換する
     );
   }
 }
